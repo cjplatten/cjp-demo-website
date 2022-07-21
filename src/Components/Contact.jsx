72a3d6a0fc8b239103572399890eac5a36c766b3
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-07-21 15:00:14 +0100

    add info to Contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import InfoCard from "./InfoCard";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h2>Contact</h2>
+      <section className="infocard-container">
+        <InfoCard>
+          <h4>Email</h4>
+          <p>cjplatten@gmail.com</p>
+        </InfoCard>
+        <InfoCard>
+          <h4>LinkedIn Profile</h4>
+          <a
+            href="https://www.linkedin.com/in/carrie-platten-8269315a/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <p>Carrie Platten</p>
+          </a>
+        </InfoCard>
+        <InfoCard>
+          <h4>Github</h4>
+          <a
+            href="https://github.com/cjplatten"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <p>cjplatten</p>
+          </a>
+        </InfoCard>
+      </section>
+    </div>
+  );
 }
